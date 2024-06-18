c0dc4e0decd9014a772b4762b519da24d8b25d31
songchangwei <1444675263@qq.com>
2024-06-18 20:43:57 +0800

    Update dcm2nii.py

--- dcm2nii.py
+++ dcm2nii.py
@@ -28,17 +28,16 @@ def dcm2nii(filepath,target_filepath):
 if __name__ == '__main__':
     filepath = sys.argv[1]
     target_filepath = sys.argv[2]
-    #filepath =  "path_to_input_dir"  #the downloaded raw files are stored in the specified path.
-    #target_filepath = 'path_to_output_dir' #the NIfTI files are saved in the designated path.
+    #filepath =  "rsna-intracranial-hemorrhage-detection/rsna-intracranial-hemorrhage-detection/stage_2_train"  #the downloaded raw files are stored in the specified path.
+    #target_filepath = 'data' #the NIfTI files are saved in the designated path.
     with jsonlines.open('annotion_file_info.jsonl','r') as reader:
-        for item in jsonlines.Reader(reader):
-            scan_name = item['scan']
+        for item in reader:
+            scan_name = item['scan_']
             slice_list = item['slice_list']
-            os.mkdir(scan_name) #A temporary file has been created.
+            if not os.path.exists(scan_name):
+                os.mkdir(scan_name) #A temporary file has been created.
             for slice in slice_list:
-                source_file = os.path.join(filepath,slice+'dcm')
-                target_file = os.path.join(scan_name,slice+'dcm')
+                source_file = os.path.join(filepath,'ID_'+slice+'.dcm')
+                target_file = os.path.join(scan_name,'ID_'+slice+'.dcm')
                 shutil.copyfile(source_file,target_file)
             dcm2nii(scan_name,os.path.join(target_filepath,scan_name+'.nii.gz'))
-
-            
